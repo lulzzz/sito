46281e3315831a66170c3db4701ffcb29283c9e2
Matteo Fabbri <matteo@phascode.org>
2018-02-15 18:20:36 +0100

    spacchettare

--- Maddalena/Startup.cs
+++ Maddalena/Startup.cs
@@ -26,21 +26,14 @@ namespace Maddalena
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddIdentity<ApplicationUser, MongoIdentityRole>(options =>
-                {
-                    options.Password.RequiredLength = 6;
-                    options.Password.RequireLowercase = false;
-                    options.Password.RequireUppercase = false;
-                    options.Password.RequireNonAlphanumeric = false;
-                    options.Password.RequireDigit = false;
-                })
-                .AddRoleStore<RoleStore<ApplicationUser>>()
-                .AddUserStore<UserStore<ApplicationUser>>()
-                .AddDefaultTokenProviders();
-
-            // Identity Services
-            services.AddTransient<IUserStore<ApplicationUser>, UserStore<ApplicationUser>>();
-            services.AddTransient<IRoleStore<MongoIdentityRole>, RoleStore<ApplicationUser>>();
+            services.AddMongoIdentityProvider<ApplicationUser>(options =>
+            {
+                options.Password.RequiredLength = 6;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireDigit = false;
+            });
 
             // Add application services.
             services.AddTransient<IEmailSender, EmailSender>();
