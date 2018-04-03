c3186abc552063c2942f19ec7ae7f5b9ec6d3b8e
Matteo Fabbri <matteo@phascode.org>
2018-04-03 02:44:49 +0200

    mn

---
+++ Maddalena/Models/UserManageModels/UserManageModel.cs
@@ -0,0 +1,15 @@
+﻿using Maddalena.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Maddalena.Models.UserManageModels
+{
+    public class UserManageModel
+    {
+        public Dictionary<string,int> RoleCount { get; set; }
+
+        public IQueryable<ApplicationUser> Users { get; set; }
+    }
+}
