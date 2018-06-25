32a0c370126fb0e271f15b5e6e6d9b7c6d027eb5
Matteo Fabbri <matteo@phascode.org>
2018-06-26 01:17:15 +0200

    sss

---
+++ Maddalena/Controllers/SalviniController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Maddalena.Models.Salvini;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using Mongolino;
+
+namespace Maddalena.Controllers
+{
+    public class SalviniController : Controller
+    {
+        static IMongoCollection<MailMessage> collection;
+
+        static SalviniController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Maddalena/Program.cs
+++ Maddalena/Program.cs
@@ -3,11 +3,50 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using System.Linq;
 using System.Runtime.InteropServices.ComTypes;
+using MongoDB.Driver;
+using Maddalena.Models.Salvini;
+using MimeKit;
+using System;
 
 namespace Maddalena
 {
     public class Program
     {
+        static void Load()
+        {
+            var collection = (new MongoClient("mongodb://localhost/"))
+                 .GetDatabase("maddalena")
+                 .GetCollection<MailMessage>("salvini");
+
+            var directory = @"C:\Users\Administrator\Downloads\New folder\";
+
+            foreach (var item in Directory.GetFiles(directory, "*.eml", SearchOption.AllDirectories))
+            {
+                try
+                {
+                    var msg = MimeKit.MimeMessage.Load(item);
+                    var body = string.Join("\r\n", msg.BodyParts.OfType<TextPart>().Select(x => x.Text).ToArray());
+                    var from = string.Join(";", msg.From.Select(x => x.ToString()));
+                    var to = msg.From.Select(x => x.ToString()).ToArray();
+
+                    collection.InsertOne(new MailMessage
+                    {
+                        Body = body,
+                        From = from,
+                        To = to,
+                        Subject = msg.Subject,
+                        FilePath = item
+                    });
+
+                    Console.WriteLine(item);
+                }
+                catch (Exception)
+                {
+                }
+            }
+
+        }
+
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
