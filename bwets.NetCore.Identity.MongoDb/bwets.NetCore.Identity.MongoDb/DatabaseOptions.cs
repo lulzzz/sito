dc22fcc5711e3cc37ad6e73885f46b75b772dec2
Matteo Fabbri <matteo@phascode.org>
2018-09-06 17:04:49 +0200

    fdfsfds

---
+++ bwets.NetCore.Identity.MongoDb/bwets.NetCore.Identity.MongoDb/DatabaseOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace bwets.NetCore.Identity.MongoDb
+{
+	public class DatabaseOptions
+	{
+		public string ConnectionString { get; set; }
+		public string UsersCollection { get; set; } = "Users";
+		public string RolesCollection { get; set; } = "Roles";
+	}
+}
