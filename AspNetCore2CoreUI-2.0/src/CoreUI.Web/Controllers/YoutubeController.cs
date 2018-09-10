85a920484cb4681d6d0cda12fd06a474d2378e81
Matteo Fabbri <matteo@phascode.org>
2018-09-10 18:30:15 +0200

    culo

---
+++ AspNetCore2CoreUI-2.0/src/CoreUI.Web/Controllers/YoutubeController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using YoutubeExplode;
+using YoutubeExplode.Models;
+using YoutubeExplode.Models.ClosedCaptions;
+
+namespace CoreUI.Web.Controllers
+{
+    public class YoutubeController : Controller
+    {
+        public async Task<ActionResult> Index(string url)
+        {
+            if (url == null) return View();
+
+            var client = new YoutubeClient();
+            if (!YoutubeClient.TryParseVideoId(url, out var videoId))
+            {
+                return View();
+            }
+
+            var minfo = await client.GetVideoMediaStreamInfosAsync(videoId);
+            return View(minfo);
+        }
+
+        [Produces("application/json")]
+        [Route("api/videoOfChannel/{channel}")]
+        public async Task<IEnumerable<Video>> VideoOfChannel(string channel)
+        {
+            var client = new YoutubeClient();
+            var list = await client.GetChannelUploadsAsync(channel);
+            return list;
+        }
+
+        [Produces("application/json")]
+        [Route("api/subtitles/{video}")]
+        public async Task<IEnumerable<ClosedCaptionTrackInfo>> Subtitles(string video)
+        {
+            var client = new YoutubeClient();
+            return await client.GetVideoClosedCaptionTrackInfosAsync(video);
+        }
+
+        [Produces("application/json")]
+        [Route("api/subtitles/{video}/{n}")]
+        public async Task<ClosedCaptionTrack> Subtitles(string video, int n)
+        {
+            var client = new YoutubeClient();
+            var info = await client.GetVideoClosedCaptionTrackInfosAsync(video);
+            return await client.GetClosedCaptionTrackAsync(info[n]);
+        }
+    }
+}
---
+++ Maddalena.Core/Extensions/LongExtensions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maddalena.Core.Extensions
+{
+    public static class LongExtensions
+    {
+        static readonly string[] BaudSuffixes = { "b/s", "Kb/s", "Mb/s", "Gb/s", "Tb/s", "Pb/s", "Eb/s", "Zb/s", "Yb/s" };
+        static readonly string[] BytesSuffixes = { "bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
+
+        public static string ToBaud(this long value, uint decimalPlaces = 0)
+        {
+            if (value < 0) { return "-" + ToBaud(-value); }
+            if (value == 0) { return string.Format("{0:n" + decimalPlaces + "} bytes", 0); }
+
+            // mag is 0 for bytes, 1 for KB, 2, for MB, etc.
+            int mag = (int)Math.Log(value, 1024);
+
+            // 1L << (mag * 10) == 2 ^ (10 * mag) 
+            // [i.e. the number of bytes in the unit corresponding to mag]
+            decimal adjustedSize = (decimal)value / (1L << (mag * 10));
+
+            // make adjustment when the value is large enough that
+            // it would round up to 1000 or more
+            if (Math.Round(adjustedSize, (int)decimalPlaces) >= 1000)
+            {
+                mag += 1;
+                adjustedSize /= 1024;
+            }
+
+            return string.Format("{0:n" + decimalPlaces + "} {1}",
+                adjustedSize,
+                BaudSuffixes[mag]);
+        }
+
+        public static string ToByteSize(this long value, uint decimalPlaces = 0)
+        {
+            if (value < 0) { return "-" + ToByteSize(-value); }
+            if (value == 0) { return string.Format("{0:n" + decimalPlaces + "} bytes", 0); }
+
+            // mag is 0 for bytes, 1 for KB, 2, for MB, etc.
+            int mag = (int)Math.Log(value, 1024);
+
+            // 1L << (mag * 10) == 2 ^ (10 * mag) 
+            // [i.e. the number of bytes in the unit corresponding to mag]
+            decimal adjustedSize = (decimal)value / (1L << (mag * 10));
+
+            // make adjustment when the value is large enough that
+            // it would round up to 1000 or more
+            if (Math.Round(adjustedSize, (int)decimalPlaces) >= 1000)
+            {
+                mag += 1;
+                adjustedSize /= 1024;
+            }
+
+            return string.Format("{0:n" + decimalPlaces + "} {1}",
+                adjustedSize,
+                BytesSuffixes[mag]);
+        }
+    }
+}
