6c49630f4bf17526d61c5efa3f9f57594b7dc23e
Matteo Fabbri <matteo@phascode.org>
2018-10-09 20:07:39 +0200

    asdsad

---
+++ VV/VV/Listen.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Speech.Recognition;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VV
+{
+    class Listen
+    {
+        static SpeechRecognitionEngine sre;
+
+        public static void Do(CultureInfo culture)
+        {
+            var l = SpeechRecognitionEngine.InstalledRecognizers();
+
+            sre = new SpeechRecognitionEngine(culture);
+            sre.SetInputToDefaultAudioDevice();
+
+            sre.SpeechRecognized += sre_SpeechRecognized;
+
+            GrammarBuilder startStop = new GrammarBuilder();
+
+            startStop.Append("Ciao");
+            startStop.Append();
+            startStop.Append("Addio");
+            Grammar grammar = new Grammar(startStop);
+            grammar.Enabled = true;
+            grammar.Name = " Free-Text Dictation ";
+            sre.LoadGrammarAsync(grammar);
+            sre.LoadGrammarCompleted += Sre_LoadGrammarCompleted;
+
+            sre.RecognizeAsync(RecognizeMode.Multiple); // multiple grammars
+        }
+
+        private static void Sre_LoadGrammarCompleted(object sender, LoadGrammarCompletedEventArgs e)
+        {
+        }
+
+        static void sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            Console.WriteLine(e.Result.Text);
+
+            string txt = e.Result.Text;
+            float confidence = e.Result.Confidence; // consider implicit cast to double
+        }
+    }
+}
---
+++ VV/VV/Speak.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VV
+{
+    class Speak
+    {
+        static SpeechSynthesizer ss = new SpeechSynthesizer();
+
+        public static void Do(CultureInfo culture)
+        {
+            var k = ss.GetInstalledVoices();
+
+            ss.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Teen, 0, culture);
+            ss.SetOutputToDefaultAudioDevice();
+            var v = ss.GetInstalledVoices().ToArray();
+
+            Console.WriteLine("\n(Speaking: I am awake)");
+            ss.Speak("Ciao patato, sono Maddalena");
+
+        }
+    }
+}
