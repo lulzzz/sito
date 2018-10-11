f70001ab670f86d1c1eead8719d959870c536ecc
Matteo Fabbri <matteo@phascode.org>
2018-10-11 17:57:50 +0200

    fdsfsfsf

---
+++ CoreUI.Web/Controllers/FeedController.cs
@@ -0,0 +1,71 @@
+﻿using System.Threading.Tasks;
+using Maddalena.Core.Feeds;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreUI.Web.Controllers
+{
+    [Authorize(Roles = "feed")]
+    public class FeedController : Controller
+    {
+        private readonly IFeedService _feed;
+
+        public FeedController(IFeedService feed)
+        {
+            _feed = feed;
+        }
+
+        // GET: Feed
+        public async Task<ActionResult> Index() => View(await _feed.All());
+
+        // GET: Feed/Create
+        public ActionResult Create() => View();
+
+        [HttpGet]
+        public async Task<ActionResult> Edit(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return View(new Feed());
+            }
+
+            var post = await _feed.ById(id);
+
+            if (post != null)
+            {
+                return View(post);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(Feed feed)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Edit", feed);
+            }
+
+            if (string.IsNullOrEmpty(feed.Id))
+            {
+                await _feed.Create(feed);
+            }
+            else
+            {
+                await _feed.Update(feed);
+            }
+
+            return Redirect("/feed");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(string id)
+        {
+            await _feed.Delete(await _feed.ById(id));
+            return Redirect("/feed");
+        }
+    }
+}
--- Maddalena.Core/Feeds/MongoFeedService.cs
+++ Maddalena.Core/Feeds/MongoFeedService.cs
@@ -18,8 +18,14 @@ namespace Maddalena.Core.Feeds
 
         public Task<IEnumerable<Feed>> All() => _feeds.WhereAsync(x => true);
 
+        public Task<IEnumerable<Feed>> All(string category) => _feeds.WhereAsync(x => x.Category == category);
+
+        public async Task<IEnumerable<string>> Categories() => (await _feeds.DistinctAsync(x => x.Category, x => true)).ToEnumerable();
+
         public Task Update(Feed feed) => _feeds.ReplaceOneAsync(x=>x.Id == feed.Id, feed);
 
         public Task Delete(Feed feed) => _feeds.DeleteOneAsync(x => x.Id == feed.Id);
+
+        public Task<Feed> ById(string id) => _feeds.FirstOrDefaultAsync(x => x.Id == id);
     }
 }
